d4797d388247a516aab1a50e36c69797d1b68c8a
yfby <harveykyleybanez@gmail.com>
2026-08-15 16:15:58 +0800

    smoother gun rotation, made it so the cannon has to rotate towards the mouse instead of instantly pointing at it

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -7,20 +7,24 @@ const BULLET = preload("res://scenes/bullet.tscn")
 # Charge time settings
 @export var min_charge_to_release := 0.2  # below this = "tap", no charge
 @export var max_charge_time := 1.5  # seconds to reach full charge
+@export var gun_rotation_speed: float = 15.0  # higher = snappier, lower = smoother
 
 var is_charging := false
 var charge_time := 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# this is allowing the gun to look at and follow the mouse thus aiming
-	look_at(get_global_mouse_position())
+func _process(delta):
+	# Calculate the angle to the mouse instead of instantly looking at it
+	var target_angle = (get_global_mouse_position() - global_position).angle()
 	
-	# This part is giving you a min and max rotation of 0 up to 360.
+	# Smoothly rotate toward that angle
+	rotation = lerp_angle(rotation, target_angle, gun_rotation_speed * delta)
+	
+	# Keep rotation in 0-360 range
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 	
-	# This part is making it so if the rotation of the gun either gets to 90 it will flip the sprite to face the other way and same with 270 degrees. 
-	if rotation_degrees > 90 and rotation_degrees< 270:
+	# Flip sprite when facing left
+	if rotation_degrees > 90 and rotation_degrees < 270:
 		scale.y = -1
 	else:
 		scale.y = 1
