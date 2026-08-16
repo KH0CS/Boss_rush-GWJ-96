282154d794b6cab1265a5d03707f0542b40f18ee
yfby <harveykyleybanez@gmail.com>
2026-08-16 18:16:52 +0800

    waiting for merge

---
+++ scripts/ui/hud.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var charge_bar = $ChargeBar
+
+func _ready():
+	# Find the Player in the scene tree
+	# await get_tree().node_added
+	# var player = get_tree().root.get_node_or_null("World/Player/Cannon")
+	# if player:
+	#	player.charge_changed.connect(_set_charge)
+	
+	print("ui loaded")
